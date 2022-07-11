886584fe1e06b90c87424765af0ab29b12d8f6d1
dawidpiegzastudia <dawid.piegza@microsoft.wsei.edu.pl>
2022-07-11 09:40:29 +0200

    DbContext and Migration for Product Model

---
+++ ProductApi/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductApi.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Category { get; set; }
+    }
+}
