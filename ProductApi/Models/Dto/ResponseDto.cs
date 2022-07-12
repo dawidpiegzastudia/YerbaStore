1d4d303e8c8207a9b1d531022a6d676b324e87fa
dawidpiegzastudia <dawid.piegza@microsoft.wsei.edu.pl>
2022-07-12 22:12:35 +0200

    Product Dto and Repository

---
+++ ProductApi/Models/Dto/ResponseDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductApi.Models.Dto
+{
+    public class ResponseDto
+    {
+        public bool IsSuccess { get; set; }
+        public object Result { get; set; }
+        public string DisplayMessege { get; set; }
+        public List<string> ErrorMessege { get; set; }
+    }
+}
---
+++ ProductApi/Repository/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using ProductApi.Models.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductApi.Repository
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<ProductDto>> GetProducts();
+        Task<ProductDto> GetProductById(int productId);
+        Task<ProductDto> CreateUpdateProduct(ProductDto productDto);
+        Task<bool> DeleteProduct(int productId);
+    }
+}
