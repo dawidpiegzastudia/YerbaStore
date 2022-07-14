5dc05e16dd32662bf60b9f882b4c7548c4523baa
dawidpiegzastudia <dawid.piegza@microsoft.wsei.edu.pl>
2022-07-14 21:29:29 +0200

    CRUD for Product in Front

---
+++ YerbaFrontEnd/Serivces/IServices/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YerbaFrontEnd.Models;
+
+namespace YerbaFrontEnd.Serivces.IServices
+{
+    public interface IProductService
+    {
+        Task<T> GetAllProductsAsync<T>();
+        Task<T> GetProductByIdAsync<T>(int id);
+        Task<T> CreateProductAsync<T>(ProductDto productDto);
+        Task<T> UpdateProductAsync<T>(ProductDto productDto);
+        Task<T> DeleteProductAsync<T>(int id);
+    }
+}
