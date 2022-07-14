5b62827b11781bf5aa2e1290161b38383dca4c57
dawidpiegzastudia <dawid.piegza@microsoft.wsei.edu.pl>
2022-07-14 22:09:57 +0200

    BaseService for FrontEnd

---
+++ YerbaFrontEnd/Serivces/IServices/IBaseService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YerbaFrontEnd.Models;
+
+namespace YerbaFrontEnd.Serivces.IServices
+{
+    public interface IBaseService : IDisposable
+    {
+        ResponseDto responseModel { get; set; }
+        Task<T> SendAsync<T>(ApiRequest apiRequest);
+    }
+}
